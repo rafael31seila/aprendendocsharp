eab5067249fd2f8afc3d0776fddf4e956e1c5e13
rafael31seila <rafaeldiastavaresdeandrade@gmail.com>
2024-08-13 16:09:07 -0300

    aula rodrigo

---
+++ cap02_ex01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cap02_ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+
+            Console.Write("Entre seu nome:");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Ola, " + Nome);
+
+            Console.WriteLine();
+            Console.WriteLine("Tecle <Enter> para encerrar...");
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
